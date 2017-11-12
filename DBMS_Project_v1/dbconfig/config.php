fecf5c587202f7ad4d4ca6287a6658cc2d2232c9
charan250498 <charan2598@gmail.com>
2017-11-12 23:50:19 +0530

    Update config.php

--- DBMS_Project_v1/dbconfig/config.php
+++ DBMS_Project_v1/dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","casper$250498") or die("Unable to connect");
+$con = mysqli_connect("localhost","root","*************") or die("Unable to connect");
 mysqli_select_db($con,"PROJECT_TRACKING");
  ?>
